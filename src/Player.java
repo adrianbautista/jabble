bd70cc4cf61eee4770d086ae3d060a6dbcfb1d0a
Adrian Bautista <adrianbautista8@gmail.com>
2013-08-26 19:30:45 -0400

    setWordsPlayed method

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,15 @@
-/**
- * Created with IntelliJ IDEA.
- * User: cgoodmac
- * Date: 8/26/13
- * Time: 1:04 AM
- * To change this template use File | Settings | File Templates.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player {
     private String name;
     private int score = 0;
-
+    private HashMap wordsPlayed;
 
    /* constructor */
    public Player(String name) {
-       name = name;
+       this.name = name;
+       this.wordsPlayed = new HashMap();
    }
 
     public int getScore() {
@@ -21,5 +18,26 @@ public class Player {
 
     public String getName() {
         return name;
+    }
+
+    public void setWordsPlayed(String word, HashMap pointGuide) {
+      char[] letters = word.toCharArray();
+      int wordLength = letters.length;
+      int wordScore = 0;
+      for (int i = 0; i < wordLength; i++) {
+        int pointValue = (int) pointGuide.get(letters[i]);
+        wordScore = wordScore + pointValue;
+      }
+
+      if (this.wordsPlayed.containsKey(word)) {
+        // in the instance that a player is playing the same word a second time on the board...
+        ArrayList<Integer> scoresArray = (ArrayList<Integer>) this.wordsPlayed.get(word);
+        scoresArray.add(new Integer(wordScore));
+        this.wordsPlayed.put(word, scoresArray);
+      } else {
+        ArrayList<Integer> scoresArray = new ArrayList<Integer>();
+        scoresArray.add(new Integer(wordScore));
+        this.wordsPlayed.put(word, scoresArray);
+      }
     }
 }
